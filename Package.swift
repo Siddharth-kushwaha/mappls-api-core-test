6c4a0f1e02ca9dac9e263f657e14df3cf9ce2db0
siddharth <siddharth.k@mapmyindia.com>
2025-06-04 14:57:04 +0530

    pushed latest fix on github

--- Package.swift
+++ Package.swift
@@ -1,21 +1,19 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let checksum = "32c991c4761ed50efd5b0c68f011eed5619d953421ae129130d945f4b63679fd"
-let version = "1.0.0"
 let package = Package(
     name: "MapplsAPICore",
     products: [
-        .library(name: "MapplsAPICore", targets: ["MapplsAPICore"])
+        .library(
+            name: "MapplsAPICore",
+            targets: ["MapplsAPICore"])
     ],
     dependencies: [
-        
     ],
     targets: [
         .binaryTarget(
             name: "MapplsAPICore",
-            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-api-core/\(version)/MapplsAPICore.xcframework.zip",
-            checksum: checksum
-        )
+            url: "https://mmi-api-team.s3.amazonaws.com/mappls-sdk-ios/mappls-api-core/MapplsAPICore-2.0.0.zip",
+            checksum: "22434cad01f10ce6680328a257a9928a18905ff52f4028dc4c6827ffe44c3626")
     ]
 )
